917645436fad527e892d4bff7b6086bba67fcc77
MuhabGamal <muhabme@gmail.com>
2024-01-03 23:37:38 +0200

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+
+try:
+    if number > 0:
+        print(f"{number} is positive")
+    elif number == 0:
+        print(f"{number} is zero")
+    else:
+        print(f"{number} is negative")
+except TypeError:
+    print("TypeError")
