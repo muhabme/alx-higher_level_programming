74553e6babec0bd1a7f202e0502d9693f03521f9
MuhabGamal <muhabme@gmail.com>
2024-04-10 00:39:56 +0200

    js oop

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const entries = Object.entries(dict);
-const values = Object.values(dict);
-const uniqueValues = [...new Set(values)];
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
 const newDict = {};
-for (const j in uniqueValues) {
+for (const j in valsUniq) {
   const list = [];
-  for (const k in entries) {
-    if (entries[k][1] === uniqueValues[j]) {
-      list.unshift(entries[k][0]);
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
     }
   }
-  newDict[uniqueValues[j]] = list;
+  newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
